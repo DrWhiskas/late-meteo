2f15dfac85cd2dd55f0b181d7e85dd628881acd5
unknown <kayv@hotmail.fr>
2024-03-02 19:12:42 +0100

    add weathers and localisations

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import '../styles/meteo.css'
 
-export default function Meteo(){
-    return(
-        <>
-        
-        </>
-    )
+export default function Meteo() {
+	const [weatherData, setWeatherData] = useState(null);
+    const [temp, setTemp] = useState(null)
+    const [city, setCity] = useState('Paris')
+
+
+
+	useEffect(() => {
+		async function fetchWeather() {
+			const url =
+				'https://weather-api138.p.rapidapi.com/weather?city_name='+ city;
+			const options = {
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Key':
+						'0312da96d3mshbfb763dd0359808p1a6cc3jsn11f261ccb01a',
+					'X-RapidAPI-Host': 'weather-api138.p.rapidapi.com',
+				},
+			};
+			try {
+				const response = await fetch(url, options);
+				const result = await response.json();
+                const convertDegree = Math.floor(result.main.temp - 273.15);
+                setWeatherData(result)
+                setTemp(convertDegree)
+                
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		fetchWeather();
+	}, [city]);
+
+    if(!weatherData){
+        return <div className='loading'>Loading...</div>
+    }
+	return (
+		<div className="meteo">
+			<div className="meteo__container">
+				<div className="meteo__container__localisation">
+					{weatherData.name
+						.replace('Arrondissement de', '')
+						.replace("Arrondissement d'", '')}
+					{temp}°C
+				</div>
+
+				<select name="city" id="city" onChange={(e) => setCity(e.target.value)}>
+					<option value="Paris">Paris</option>
+					<option value="Lyon">Lyon</option>
+					<option value="Argenteuil">Argenteuil</option>
+					<option value="Bordeaux">Bordeaux</option>
+					<option value="Nice">Nice</option>
+					<option value="Nantes">Nantes</option>
+					<option value="Toulouse">Toulouse</option>
+					<option value="Lille">Lille</option>
+					<option value="Strasbourg">Strasbourg</option>
+				</select>
+			</div>
+		</div>
+	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Time from '../components/Time';
+import Meteo from '../components/Meteo';
 import '../styles/home.css';
 
 export default function Home() {
@@ -8,7 +9,38 @@ export default function Home() {
 			<div className="stars"></div>
 			<div className="twinkling"></div>
 			<div className="clouds"></div>
-			<Time />
+			<div className="home__weather">
+				<div className="home__weather__container">
+					<Meteo />
+					<Time />
+				</div>
+			</div>
 		</div>
 	);
 }
+
+/*
+
+
+.time{
+    width: 100%;
+    height: 100svh;
+    position: absolute;
+    z-index: 4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.time__container{
+    width: 50%;
+    height: 30vh;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    font-size: calc(30% + 8svw);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+*/
